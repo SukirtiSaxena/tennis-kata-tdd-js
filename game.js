c83742d35ebbb058c4077504d24d5e2b5ec7bd77
Sukirti Saxena <91497529+SukirtiSaxena@users.noreply.github.com>
2022-01-29 14:14:57 +0000

    refactored code

--- game.js
+++ game.js
@@ -7,23 +7,20 @@ const tennis = scores => {
         3: '40'
     };
 
-    if (scores[0] > 3 || scores[1] > 3) {
-        if (scores[0] === scores[1] + 1)
-            return 'Player One is at Advantage';
-        if (scores[1] === scores[0] + 1)
-            return 'Player Two is at Advantage';
-        if (scores[0] >= scores[1] + 2)
-            return 'Player One is the Winner';
-        if (scores[1] >= scores[0] + 2)
-            return 'Player Two is the Winner';
-        if (scores[0] === scores[1])
-            return 'Deuce';
-    }
-    else {
-        if (scores[0] === scores[1])
-            return (scoring[scores[0]] + ' - All');
-        return (scoring[scores[0]] + ' - ' + scoring[scores[1]]);
-    }
+    let finalScore;
+    
+    (scores[0] > 3 || scores[1] > 3) ? 
+       (scores[0] === scores[1] + 1) ? finalScore = 'Player One is at Advantage' :
+            (scores[1] === scores[0] + 1) ? finalScore = 'Player Two is at Advantage' :
+                (scores[0] >= scores[1] + 2) ? finalScore = 'Player One is the Winner' :
+                    (scores[1] >= scores[0] + 2) ? finalScore = 'Player Two is the Winner' :
+                        (scores[0] === scores[1]) ? finalScore = 'Deuce' : finalScore = ''
+        :
+        (scores[0] === scores[1]) ?
+            finalScore = (scoring[scores[0]] + ' - All') :
+            finalScore = (scoring[scores[0]] + ' - ' + scoring[scores[1]]);
+
+            return finalScore;
 };
 
 module.exports = { tennis };
