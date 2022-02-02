c6ccd1ea59fef5bc5d304dd568493f5c8550cd8d
Sukirti Saxena <91497529+SukirtiSaxena@users.noreply.github.com>
2022-02-02 16:56:51 +0000

    refactored code, added edge cases check

--- game.js
+++ game.js
@@ -1,5 +1,9 @@
 
 const tennis = scores => {
+    if (scores[0] === undefined || scores[1] === undefined) return "Number is required";
+    if (!Number.isInteger(scores[0]) || !Number.isInteger(scores[1])) return 'Not a number';
+    if (scores[0] < 0 || scores[1] < 0) return 'Scores cannot be negative!!';
+
     const scoring = {
         0: 'love',
         1: '15',
@@ -8,19 +12,26 @@ const tennis = scores => {
     };
 
     let finalScore;
-    
-    (scores[0] > 3 || scores[1] > 3) ? 
-       (scores[0] === scores[1] + 1) ? finalScore = 'Player One is at Advantage' :
-            (scores[1] === scores[0] + 1) ? finalScore = 'Player Two is at Advantage' :
-                (scores[0] >= scores[1] + 2) ? finalScore = 'Player One is the Winner' :
-                    (scores[1] >= scores[0] + 2) ? finalScore = 'Player Two is the Winner' :
-                        (scores[0] === scores[1]) ? finalScore = 'Deuce' : finalScore = ''
-        :
-        (scores[0] === scores[1]) ?
-            finalScore = (scoring[scores[0]] + ' - All') :
-            finalScore = (scoring[scores[0]] + ' - ' + scoring[scores[1]]);
 
-            return finalScore;
+    if (scores[0] > 3 || scores[1] > 3) {
+        if (scores[0] === scores[1] + 1)
+            finalScore = 'Player One is at Advantage';
+        if (scores[1] === scores[0] + 1)
+            finalScore = 'Player Two is at Advantage';
+        if (scores[0] >= scores[1] + 2)
+            finalScore = 'Player One is the Winner';
+        if (scores[1] >= scores[0] + 2)
+            finalScore = 'Player Two is the Winner';
+        if (scores[0] === scores[1])
+            finalScore = 'Deuce';
+    }
+    else if (scores[0] === scores[1]) {
+        finalScore = (scoring[scores[0]] + ' - All');
+    }
+    else
+        finalScore = (scoring[scores[0]] + ' - ' + scoring[scores[1]]);
+
+    return finalScore;
 };
 
 module.exports = { tennis };
