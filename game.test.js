708081450a890320560e93e5d8a5f89a7a006a36
Sukirti Saxena <91497529+SukirtiSaxena@users.noreply.github.com>
2022-02-02 16:57:30 +0000

    Added edge test cases

--- game.test.js
+++ game.test.js
@@ -13,13 +13,13 @@ describe("Tennis Kata Test Cases", () => {
         [[1, 2], '15 - 30'],
         [[2, 1], '30 - 15'],
         [[2, 3], '30 - 40'],
-        [[3, 2], '40 - 30'], 
-        [[4, 2], 'Player One is the Winner'],    
-        [[2, 4], 'Player Two is the Winner'], 
+        [[3, 2], '40 - 30'],
+        [[4, 2], 'Player One is the Winner'],
+        [[2, 4], 'Player Two is the Winner'],
         [[4, 6], 'Player Two is the Winner'],
         [[4, 4], 'Deuce'],
         [[5, 4], 'Player One is at Advantage'],
-        [[5, 6], 'Player Two is at Advantage'],    
+        [[5, 6], 'Player Two is at Advantage'],
         [[6, 4], 'Player One is the Winner'],
         [[6, 6], 'Deuce'],
         [[9, 11], 'Player Two is the Winner']
@@ -27,5 +27,40 @@ describe("Tennis Kata Test Cases", () => {
         test("If [Player 1 Score, Player 2 Score] is %s, give message , '%s' ", (score, expected) => {
             expect(tennis(score)).toBe(expected);
         });
+
+    each([
+        [[-2, 3], 'Scores cannot be negative!!'],
+        [[3, -5], 'Scores cannot be negative!!'],
+        [[-3, -4], 'Scores cannot be negative!!']
+    ]).
+        test("If any score is in negative then give error message", (score, expected) => {
+            expect(tennis(score)).toBe(expected);
+
+        });
+
+    each([
+        [['abcd', 4], 'Not a number'],
+        [[5, 'xyz'], 'Not a number'],
+        [['abcd', 'xyz'], 'Not a number']
+    ]).
+        test("If input score %s is not a number then give error message!!", (score, expected) => {
+            expect(tennis(score)).toBe(expected);
+        });
+
+    test("If input score is undefined then give error message!!", () => {
+        expect(tennis([])).toBe('Number is required');
+    });
+
+    test("If input score is undefined then give error message!!", () => {
+        expect(tennis([,])).toBe('Number is required');
+    });
+
+    test("If input score is undefined then give error message!!", () => {
+        expect(tennis([2,])).toBe('Number is required');
+    });
+
+    test("If input score is undefined then give error message!!", () => {
+        expect(tennis([, 5])).toBe('Number is required');
+    });
 
 });
